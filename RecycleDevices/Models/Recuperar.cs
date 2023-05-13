6ae73b49b062f787a7d29da3d08d393ddd109a16
LAPTOP-LOLREGVU\Nicolas Torres <nico.rodriguez.torres.71@gmail.com>
2023-05-13 06:06:28 -0500

    cambios en ya hechos

---
+++ RecycleDevices/Models/Recuperar.cs
@@ -0,0 +1,9 @@
+﻿namespace RecycleDevices.Models
+{
+    public class Recuperar
+    {
+        public int Id { get; set; } 
+        public string? code { get; set; }
+        public string? newPassword { get; set; }
+    }
+}
